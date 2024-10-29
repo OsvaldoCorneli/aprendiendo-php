f61f002986f59fe0355558325bf7f63e46ad5416
OsvaldoCorneli <overcorneli@gmail.com>
2024-10-29 19:43:39 -0300

    por la mitad del modulo 6

--- Modulo6.php
+++ Modulo6.php
@@ -1,11 +1,30 @@
 <?php 
 
+//Array
+
+$array = ["Osvaldo", "Matias" , "Corneli"];
+
+echo $array[2];
+echo $array[1];
+foreach( $array as $index=>$nombre ) {
+
+    echo "nombre: $nombre $index";
 
 
+}
+echo "<br>";
+echo "<br>";
+//Arreglos asociativos:
 
+$datos = [
+    "nombre"=>"Osvaldo",
+    "Apellido"=>"Corneli",
+    "Edad"=>"34"
+];
 
-
-
-
+foreach($datos as $key=>$valor){
+    echo "$key: $valor";
+    echo "<br>";
+}
 
 ?>;
