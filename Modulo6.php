83e4603cc95a04da4e4a4371b5297376f75a23c2
OsvaldoCorneli <overcorneli@gmail.com>
2024-10-30 17:22:15 -0300

    todavia sigo en el modulo 6

--- Modulo6.php
+++ Modulo6.php
@@ -27,4 +27,62 @@ foreach($datos as $key=>$valor){
     echo "<br>";
 }
 
+echo "<br>";
+echo "<br>";
+
+//Agregar a array 
+
+$alumnos=["Osvaldo","Matiaz","Corneli"];
+
+$datos = [
+    "nombre"=>"Osvaldo",
+    "apellido"=>"Corneli",
+    "edad"=>"30"
+];
+echo "ARRAY";
+echo "<br>";
+var_dump($alumnos);
+echo "<br>";
+var_dump($datos);
+echo "<br>";
+
+//Agregar con push
+echo "ARRAY AGREGADO";
+echo "<br>";
+array_push($alumnos, "Nassif");
+$datos["Estudiante"]=true;
+
+var_dump($alumnos);
+echo "<br>";
+var_dump($datos);
+echo "<br>";
+
+
+//Eliminar un dato
+// echo "ARRAY ELIMINADO";
+echo "<br>";
+unset($alumnos[3]);
+
+unset($datos["edad"]);
+
+var_dump($alumnos);
+echo "<br>";
+var_dump($datos);
+echo "<br>";
+
+//Modificar datos:
+    echo "ARRAY MODIFICADO";
+    echo "<br>";
+ $alumnos[1]="Matias";   
+ $datos["edad"] = "34";
+
+ var_dump($alumnos);
+echo "<br>";
+var_dump($datos);
+echo "<br>";
+
+
+//FUNCIONES
+
+
 ?>;
