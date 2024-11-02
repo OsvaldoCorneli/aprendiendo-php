1cd24a452181ec8111bfbb3ed8fabf7bc3ee7317
OsvaldoCorneli <overcorneli@gmail.com>
2024-11-01 21:32:21 -0300

    clase 1 modulo 9 base de datos

---
+++ Modulo9.php
@@ -0,0 +1,14 @@
+<?php 
+//MODULO 9
+echo "<br>MODULO 9<br><br>";
+// Que es un dato?;
+//Que es DNMS?
+//SQL - NOSQL; - TRABAJAREMOS CON SQL
+
+//API DE DATOS 
+//PDO - Mysql - Mysqli
+
+//CLASE 2 Crear base de datos
+
+
+?>
